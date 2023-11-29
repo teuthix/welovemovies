6327f1a7a68f69b8cc701d0a36355da5aac37737
wey <pukaimakaola@gmail.com>
2023-11-28 23:44:31 -0500

    refined theaters folder files, passes 5 tests

--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -2,10 +2,36 @@ const theatersService = require("./theaters.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 async function list(req, res, next) {
-    const data = theatersService.list();
-    res.json({ data });
+    const data = await theatersService.list();
+    // console.log(data);
+    if (data){
+        res.locals.theaters = data;
+        next();
+    }
+    next({ status: 404, message: "Theater cannot be found." });
 }
 
+async function getMovies(req, res, next) {
+    const theaters = res.locals.theaters;
+    //array of theater ids
+    const theaterIds = theaters.map((theater) => {
+        return theater.theater_id;
+    });
+
+    const theaterWithMovies = await Promise.all(theaterIds.map(async (theaterId) => {
+        const movies = await theatersService.findMoviesByTheaterId(theaterId);
+        return { movies };
+    }));
+    
+    // console.log(theaterWithMovies);
+    res.json({ data: theaterWithMovies });
+}
+
+// return all theaters, each with the movies playing there
+// get all the theaters
+// then find all the movies for that theater's id
+
+
 module.exports = {
-    list: [asyncErrorBoundary(list)],
+    list: [asyncErrorBoundary(list), getMovies],
 };
--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,9 +1,27 @@
 const knex = require("../db/connection");
 
+function findMoviesByTheaterId(theater_id) {
+    return knex("theaters as t")
+        .join("movies_theaters as mt", "t.theater_id", "mt.theater_id")
+        .join("movies as m", "m.movie_id", "mt.movie_id")
+        .select("m.*")
+        .where({ "t.theater_id": theater_id });
+}
+
+// function showingTrue() {
+//     return knex("movies as m")
+//         .join("movies_theaters as mt", "m.movie_id", "mt.movie_id")
+//         .join("theaters as t", "mt.theater_id", "t.theater_id")
+//         .select("m.*")
+//         .where({ "mt.is_showing": true })
+//         .first();
+// };
+
 function list() {
     return knex("theaters").select("*");
 }
 
 module.exports = {
+    findMoviesByTheaterId,
     list,
 };
